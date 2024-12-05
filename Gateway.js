1699263b72c75618c0cdb892ee1cafdaaadbf759
Pedro-RGS <pedro.gsouza@upe.br>
2024-12-05 00:38:54 -0300

    login arrumado

--- Gateway.js
+++ Gateway.js
@@ -7,10 +7,11 @@ const app = express();
 const PORT = 8088;
 
 const SERVICES = {
-    auth: 'http://localhost:8080/api/auth', // Serviço de autenticação, 8081 ta o front (Deu um bug cabuloso aqui quando errei a porta)
-    posts: 'http://localhost:8082', // Serviço de posts
+    auth: 'https://f464-2804-29b8-524e-908-886a-49e1-9ec7-75f8.ngrok-free.app/api/auth', // Serviço de autenticação, 8081 ta o front (Deu um bug cabuloso aqui quando errei a porta)
+    base: 'http://localhost:8082/api', // Serviço de posts
 
 };
+
 
 //JWT
 const authenticate = async (req, res, next) => {
@@ -18,7 +19,7 @@ const authenticate = async (req, res, next) => {
     if (!token) {
         return res.status(401).json({ error: 'Token não fornecido' });
     }
-
+    
     try {
         // Valida o token via microsserviço de autenticação
         const response = await axios.get(`${SERVICES.auth}/validate-token`, {
@@ -50,9 +51,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.post('/api/auth/register', async (req, res) => {
+app.post('/api/auth/register' ,async (req, res) => {
     const { username, email, password } = req.body;
-
+    
     try {
         // Enviar dados ao microsserviço de autenticação
         const authResponse = await axios.post(`${SERVICES.auth}/register`, {
@@ -62,10 +63,11 @@ app.post('/api/auth/register', async (req, res) => {
         });
 
         console.log('Resposta do microsserviço de autenticação:', authResponse.status);
-
+        const usuario = authResponse.data;
 
         // Enviar os dados ao microsserviço de posts
-        const postsResponse = await axios.post(`${SERVICES.posts}/api/usuarios/register`,  {
+        const postsResponse = await axios.post(`${SERVICES.base}/usuarios/register`,  {
+            id: usuario.id,
             nome: username,
             email: email,
             senha: password,
@@ -73,6 +75,7 @@ app.post('/api/auth/register', async (req, res) => {
             seguindo: null
         });
 
+        
         console.log('Resposta do microsserviço de posts:', postsResponse.status);
         console.log(res.json);
 
@@ -90,7 +93,6 @@ app.post('/api/auth/register', async (req, res) => {
         res.status(500).json({ message: 'Erro ao registrar o usuário.' });
     }
 });
-
 
 app.use(
     '/api/auth',
@@ -115,85 +117,69 @@ app.get('/home', authenticate, (req, res) => {
     });
 });
 
-//pra que o get se tem o use depois e não precisa desse *, funciona pra pegar tudo assim: '/api
-app.get(
-    '/api/*',  // Captura qualquer requisição que comece com /api/
-    enrichRequestWithUserData,
-    createProxyMiddleware({
-        target: SERVICES.posts,  // Defina o serviço de destino para redirecionar
-        changeOrigin: true,      // Muda a origem para o servidor de destino
-        pathRewrite: (path, req) => {
-            console.log(`Reescrevendo o caminho de ${path}`);
-            return path.replace(/^\/api/, '/api');  // O caminho é mantido como está
-        },
-        onProxyReq: (proxyReq, req, res) => {
-            console.log(`Proxy headers: ${JSON.stringify(req.headers)}`);
-            console.log(`Proxy para o caminho: ${req.url}`);
-        },
-        onError: (err, req, res) => {
-            console.error(`Erro ao redirecionar para o serviço de posts: ${err.message}`);
-            res.status(500).json({ error: 'Erro ao redirecionar a requisição para o serviço de posts.' });
-        },
-    })
-);
+
+// seguir um usuário
+app.post('/api/usuarios/follow/:seguidorId/:alvoId', async (req, res) => {
+    const {seguidorId, alvoId} = req.params
+
+    try {
+        // Enviar dados ao microsserviço de autenticação
+        const baseResponse = await axios.post(`${SERVICES.base}/usuarios/follow/${seguidorId}/${alvoId}`);
+
+        console.log('Resposta do microsserviço base:', baseResponse.status);
 
 
-app.use(
-    '/api/usuarios',
-    createProxyMiddleware({
-        target: SERVICES.auth,
-        changeOrigin: true,
-        onProxyReq: (proxyReq, req, res) => {
-            console.log('Cabeçalhos recebidos pela Gateway:', req.headers);
-        },
-        onError: (err, req, res) => {
-            console.error('Erro no Proxy:', err);
-            res.status(500).send('Erro ao redirecionar a requisição.');
+        // Retornar sucesso
+        res.status(200).json(baseResponse);
+
+    } catch (error) {
+        // Capturar e exibir erro detalhado
+        console.error('Erro ao seguir o usuário:', error.message);
+        if (error.response) {
+            console.error('Resposta de erro do servidor:', error.response.data);
         }
-    })
+        res.status(500).json({ message: 'Erro ao seguir o usuário.' });
+    }
+});
+
+// criar novo post
+app.post(
+    '/posts',
+    async (req, res) => {
+        const novoPost = req.body;
+    
+        try {
+            const baseResponse = await axios.post(`${SERVICES.base}/posts`, novoPost);
+    
+            console.log('Resposta do microsserviço base:', baseResponse.status);
+    
+            res.status(201).json(baseResponse);
+    
+        } catch (error) {
+            console.error('Erro ao seguir o usuário:', error.message);
+            if (error.response) {
+                console.error('Resposta de erro do servidor:', error.response.data);
+            }
+            res.status(500).json({ message: 'Erro ao seguir o usuário.' });
+        }
+    }
 );
 
-app.use(
-    '/api/*',  // Usando um wildcard para capturar qualquer coisa que comece com /api/
-    enrichRequestWithUserData,
-    createProxyMiddleware({
-        target: SERVICES.posts, // O serviço para o qual você está redirecionando
-        changeOrigin: true,      // Muda a origem para o destino, necessário para evitar problemas de CORS
-        pathRewrite: (path, req) => {
-            console.log(`Reescrevendo o caminho de ${path}`);
-            // O path já é o que você quer no destino, então apenas retorna ele mesmo
-            return path.replace(/^\/api/, '/api');  // Pode ser ajustado se necessário
-        },
-        onProxyReq: (proxyReq, req, res) => {
-            console.log(`Proxy headers: ${JSON.stringify(req.headers)}`);
-            console.log(`Proxy para o caminho: ${req.url}`);
-        },
-        onError: (err, req, res) => {
-            console.error(`Erro ao redirecionar para o serviço de posts: ${err.message}`);
-            res.status(500).json({ error: 'Erro ao redirecionar a requisição para o serviço de posts.' });
-        },
-    })
-);
+app.get("/api/usuarios/:usuarioId", async (req, res) => {
+    const {usuarioId} = req.params;
+    
+    const baseResponse = await axios.get(`${SERVICES.base}/usuarios/${usuarioId.data}`);
 
-//pra que isso se tem o use la na frente, ele devia fazer esse trabalho já
-app.get(
-    '/api/usuarios',
-    enrichRequestWithUserData,
-    createProxyMiddleware({
-        target: SERVICES.posts,
-        changeOrigin: true,
-        pathRewrite: { '^/api/usuarios': '' },
-        onProxyReq: (proxyReq, req, res) => {
-            console.log(`Proxy headers: ${JSON.stringify(req.headers)}`);
-            console.log(`Proxy para Posts: ${req.url}`);
-        },
-        onError: (err, req, res) => {
-            console.error(`Erro ao redirecionar para o serviço de posts: ${err.message}`);
-            res.status(500).json({ error: 'Erro ao redirecionar a requisição para o serviço de posts.' });
-        },
-    })
-);
+    console.log(baseResponse.data);
+    return res.status(200).json(baseResponse.data);
+})
 
+// pegar todos os posts
+app.get("/api/posts", async (req, res) =>{
+    const baseResponse = await axios.get(`${SERVICES.base}/posts`);
+    console.log(baseResponse.data);
+    return res.status(200).json(baseResponse.data);
+});
 
 app.listen(PORT, () => {
     console.log(`API Gateway rodando na porta ${PORT}`);
